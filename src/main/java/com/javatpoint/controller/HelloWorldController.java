1948663c3becdd3783ed1ddbb6d29e1e6d74874a
WalkiePanguin <89259471+WalkiePanguin@users.noreply.github.com>
2022-06-13 17:05:37 +0800

    Update HelloWorldController.java

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController 
 {
-@RequestMapping("/")
+@RequestMapping("/webapps")
 public String hello() 
 {
 return "Hello NCS  :) this is my first demo";
