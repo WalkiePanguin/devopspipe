4fa58e963f8295c7673d437b8872a700f770ff5b
WalkiePanguin <89259471+WalkiePanguin@users.noreply.github.com>
2024-05-06 01:01:56 +0800

    Update CodeQLDemo.java

--- src/main/java/com/javatpoint/CodeQLDemo.java
+++ src/main/java/com/javatpoint/CodeQLDemo.java
@@ -4,13 +4,6 @@ import java.io.FileReader;
 
 public class CodeQLDemo {
     public static void main(String[] args) {
-        try {
-            String test = "aWKKna41COO3J1Fq772dDXqYpdPYhL3z"; // Corrected variable declaration
-            File file = new File("nonexistent_file.txt");
-            FileReader fr = new FileReader(file); // Potential FileNotFoundException
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found!");
-            e.printStackTrace();
-        }
+        System.out.println("hello");
     }
 }
