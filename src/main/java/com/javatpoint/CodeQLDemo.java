f25070dd95ac1991053e73fda10d420436d295ff
WalkiePanguin <89259471+WalkiePanguin@users.noreply.github.com>
2024-05-06 11:47:14 +0800

    Update CodeQLDemo.java

--- src/main/java/com/javatpoint/CodeQLDemo.java
+++ src/main/java/com/javatpoint/CodeQLDemo.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,25 +14,18 @@ import java.sql.ResultSet;
 
 
 public class CodeQLDemo {
-    // Hardcoded Secret Key Vulnerability
-    private static final String username = "superSecret";
-    private static final String password = "superSecret123";
-
-    private static final String Key= "b6eb2BEZ3+Utu8kscnNFVw==";
+    // // Hardcoded Secret Key Vulnerability
+    // private static final String username = "superSecret";
+    // private static final String password = "superSecret123";
 
     public static void main(String[] args) {
         System.out.println("Hello");
         
     }
-
-    
-
-   //  Cross-Site Scripting (XSS)
-   public void displayUserInput(HttpServletRequest request, HttpServletResponse response) throws IOException {
-       String userInput = request.getParameter("input");
-       response.getWriter().write("<html><body>User input: " + userInput + "</body></html>");
-   }
-
-
+   // //  Cross-Site Scripting (XSS)
+   // public void displayUserInput(HttpServletRequest request, HttpServletResponse response) throws IOException {
+   //     String userInput = request.getParameter("input");
+   //     response.getWriter().write("<html><body>User input: " + userInput + "</body></html>");
+   // }
 
 }
