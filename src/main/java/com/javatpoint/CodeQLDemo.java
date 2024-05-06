34151a8c4bd9fd0eb335fa706a3fe02f30b19952
WalkiePanguin <89259471+WalkiePanguin@users.noreply.github.com>
2024-05-06 11:24:25 +0800

    Update CodeQLDemo.java

--- src/main/java/com/javatpoint/CodeQLDemo.java
+++ src/main/java/com/javatpoint/CodeQLDemo.java
@@ -10,37 +10,18 @@ import java.util.Base64;
 
 public class CodeQLDemo {
     // Hardcoded Secret Key Vulnerability
-    //private static final String username = "superSecret";
-    //private static final String password = "superSecret123";
+    private static final String username = "superSecret";
+    private static final String password = "superSecret123";
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        // Convert the secret key string into a byte array
-        byte[] keyBytes = SECRET_KEY.getBytes();
-
-        // Create an AES SecretKey from the byte array
-        SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
-
-        String message = "This is a secret message";
-        try {
-            String encrypted = encrypt(message, secretKey);
-            System.out.println("Encrypted: " + encrypted);
-
-            String decrypted = decrypt(encrypted, secretKey);
-            System.out.println("Decrypted: " + decrypted);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
-
-
-        
     
 
-   ////  Cross-Site Scripting (XSS)
-   // public void displayUserInput(HttpServletRequest request, HttpServletResponse response) throws IOException {
-   //     String userInput = request.getParameter("input");
-   //     response.getWriter().write("<html><body>User input: " + userInput + "</body></html>");
-   // }
+   //  Cross-Site Scripting (XSS)
+   public void displayUserInput(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       String userInput = request.getParameter("input");
+       response.getWriter().write("<html><body>User input: " + userInput + "</body></html>");
+   }
 }
