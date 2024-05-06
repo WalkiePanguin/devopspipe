90b552e470fc43a475893aa0921e45e98045e48b
WalkiePanguin <89259471+WalkiePanguin@users.noreply.github.com>
2024-05-06 11:03:13 +0800

    Update CodeQLDemo.java

--- src/main/java/com/javatpoint/CodeQLDemo.java
+++ src/main/java/com/javatpoint/CodeQLDemo.java
@@ -6,4 +6,24 @@ public class CodeQLDemo {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    // Cross-Site Scripting (XSS)
+     public void displayUserInput(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userInput = request.getParameter("input");
+        response.getWriter().write("<html><body>User input: " + userInput + "</body></html>");
+    }
+
+     public static String encrypt(String data) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes("UTF-8")));
+    }
+
+    public static String decrypt(String encryptedData) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedData)));
+    }
 }
