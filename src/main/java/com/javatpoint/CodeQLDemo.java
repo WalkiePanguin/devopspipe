507e8e3bc8028f34fc81fba5691d42dafb4a7454
WalkiePanguin <89259471+WalkiePanguin@users.noreply.github.com>
2024-05-06 11:06:22 +0800

    Update CodeQLDemo.java

--- src/main/java/com/javatpoint/CodeQLDemo.java
+++ src/main/java/com/javatpoint/CodeQLDemo.java
@@ -9,17 +9,21 @@ import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 public class CodeQLDemo {
+    // Hardcoded Secret Key Vulnerability
+    private static final String SECRET_KEY = "superSecretKey123";
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello");
     }
 
     // Cross-Site Scripting (XSS)
-     public void displayUserInput(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void displayUserInput(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String userInput = request.getParameter("input");
         response.getWriter().write("<html><body>User input: " + userInput + "</body></html>");
     }
 
-     public static String encrypt(String data) throws Exception {
+    // Hardcoded Secret Key Vulnerability
+    public static String encrypt(String data) throws Exception {
         SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
@@ -31,5 +35,14 @@ public class CodeQLDemo {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedData)));
+    }
+
+    // File Not Found Exception Example
+    public void readFile(String filePath) {
+        try {
+            FileReader fileReader = new FileReader(new File(filePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
